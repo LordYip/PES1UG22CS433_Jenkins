8ddfb8245ef64bfc99840e913c953dedf5568a4a
LordYip <pranav.rao66@gmail.com>
2025-03-18 21:30:36 +0530

    New file

--- main/hello1.cpp
+++ main/hello1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "zero effort new cpp file, why is this so hard bruh";
+  cout << "zero effort new cpp file, why is this so hard bruh\n";
+  cout << "What is this";
   return 0;
 }
